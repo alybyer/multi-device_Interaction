737d8841665f709df10d863d99cffbd8d5b6a2b2
Aly Byer <alyebyer@gmail.com>
2019-02-04 16:29:26 -0500

    added json file

--- public/js/create-tree-component.js
+++ public/js/create-tree-component.js
@@ -7,13 +7,13 @@ AFRAME.registerComponent('create-tree-component', {
         Context_AF.el.addEventListener('click', function(event) {
             console.log("click");
             //object clicked - lets create a cow!
-            Context_AF.createCow();
+            Context_AF.createTree();
 
             Context_AF.soundElem.components['sound'].stopSound(); //stop first so we aren't trying to play more than once at same time
             Context_AF.soundElem.components['sound'].playSound();
         });
     },
-    createCow : function() {
+    createTree : function() {
         const Context_AF = this;
 
         //create element, than add attributes you want. If you are creating many =you want to 
